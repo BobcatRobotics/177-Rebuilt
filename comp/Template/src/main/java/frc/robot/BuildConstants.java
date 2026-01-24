d1ffbb1a14e7f7a9b149b6eb0fae5237cb21dfa8
ao <accent-truffle.9w@icloud.com>
2026-01-23 23:50:38 -0500

    Library & Swerve
    Making modules dynamic so that we can have different types of modules.

--- comp/Template/src/main/java/frc/robot/BuildConstants.java
+++ comp/Template/src/main/java/frc/robot/BuildConstants.java
@@ -7,13 +7,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Template";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = -1;
-  public static final String GIT_SHA = "UNKNOWN";
-  public static final String GIT_DATE = "UNKNOWN";
-  public static final String GIT_BRANCH = "UNKNOWN";
-  public static final String BUILD_DATE = "2026-01-12 20:25:21 EST";
-  public static final long BUILD_UNIX_TIME = 1768267521069L;
-  public static final int DIRTY = 0;
+  public static final int GIT_REVISION = 9;
+  public static final String GIT_SHA = "d52b04e67a943df17a4aefc1b8c6b23546fce517";
+  public static final String GIT_DATE = "2026-01-16 23:25:17 EST";
+  public static final String GIT_BRANCH = "main";
+  public static final String BUILD_DATE = "2026-01-23 23:47:57 EST";
+  public static final long BUILD_UNIX_TIME = 1769230077028L;
+  public static final int DIRTY = 1;
 
   private BuildConstants(){}
 }
