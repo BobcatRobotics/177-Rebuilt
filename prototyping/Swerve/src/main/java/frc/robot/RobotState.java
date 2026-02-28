cebbcab792e5919ef77c0bf0047b26ca0dd9be65
aopalka <79727669+aopalka@users.noreply.github.com>
2026-02-28 05:38:57 -0500

    Update RobotState.java

--- prototyping/Swerve/src/main/java/frc/robot/RobotState.java
+++ prototyping/Swerve/src/main/java/frc/robot/RobotState.java
@@ -1,7 +1,14 @@
+package frc.robot;
+
 public class RobotState {
     private static RobotState instance;
       public static RobotState getInstance() {
     if (instance == null) instance = new RobotState();
     return instance;
   }
+
+  // Save Odometry,
+  // Save Swerve Module Details
+  // Save Vision Tags
+
 }
