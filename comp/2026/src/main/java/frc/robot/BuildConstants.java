940a3cbc06aa27244c69de335a80ebacef04f16b
h3L1X <116317505+h3L1X@users.noreply.github.com>
2026-03-10 17:07:56 -0400

    Generated a new tuner constants file and put it in the right place

--- comp/2026/src/main/java/frc/robot/BuildConstants.java
+++ comp/2026/src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 129;
-  public static final String GIT_SHA = "5a575f53b7be5cfda6ad1a301488450cf6be1a07";
-  public static final String GIT_DATE = "2026-03-09 20:58:53 EDT";
+  public static final int GIT_REVISION = 131;
+  public static final String GIT_SHA = "4c961fc8306ed3a4d52a71e8a9c87bb898d8b4fc";
+  public static final String GIT_DATE = "2026-03-10 16:53:30 EDT";
   public static final String GIT_BRANCH = "clean-project";
-  public static final String BUILD_DATE = "2026-03-10 00:54:01 EDT";
-  public static final long BUILD_UNIX_TIME = 1773118441057L;
+  public static final String BUILD_DATE = "2026-03-10 17:02:35 EDT";
+  public static final long BUILD_UNIX_TIME = 1773176555891L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
