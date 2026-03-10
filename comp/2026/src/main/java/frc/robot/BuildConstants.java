cba7d65d2da3e8c5476e4e081bea4fa5a5377072
h3L1X <116317505+h3L1X@users.noreply.github.com>
2026-03-10 19:49:42 -0400

    Added Vision Subsystem and checked with the boot test and works fine.

--- comp/2026/src/main/java/frc/robot/BuildConstants.java
+++ comp/2026/src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 132;
-  public static final String GIT_SHA = "940a3cbc06aa27244c69de335a80ebacef04f16b";
-  public static final String GIT_DATE = "2026-03-10 17:07:56 EDT";
+  public static final int GIT_REVISION = 133;
+  public static final String GIT_SHA = "c4d6ed022390b456b110d2caaf7970b82cdc68c8";
+  public static final String GIT_DATE = "2026-03-10 19:32:48 EDT";
   public static final String GIT_BRANCH = "clean-project";
-  public static final String BUILD_DATE = "2026-03-10 19:27:21 EDT";
-  public static final long BUILD_UNIX_TIME = 1773185241707L;
+  public static final String BUILD_DATE = "2026-03-10 19:42:47 EDT";
+  public static final long BUILD_UNIX_TIME = 1773186167393L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
