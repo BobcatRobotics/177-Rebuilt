a3fc2e2891a78b2071542376814b851c7a896e81
ao <accent-truffle.9w@icloud.com>
2026-03-12 16:18:46 -0500

    Commented out visual vm calls in build.gradle.

--- comp/2026/src/main/java/frc/robot/BuildConstants.java
+++ comp/2026/src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 141;
-  public static final String GIT_SHA = "a76accb29f5f388abf237eec6d03c15a248b08f8";
-  public static final String GIT_DATE = "2026-03-12 16:41:19 EDT";
+  public static final int GIT_REVISION = 143;
+  public static final String GIT_SHA = "9d23fa2e3a28f70da831cbcdac39cabe7ba3afed";
+  public static final String GIT_DATE = "2026-03-12 17:03:20 EDT";
   public static final String GIT_BRANCH = "clean-project";
-  public static final String BUILD_DATE = "2026-03-12 16:42:23 EDT";
-  public static final long BUILD_UNIX_TIME = 1773348143784L;
+  public static final String BUILD_DATE = "2026-03-12 17:18:13 EDT";
+  public static final long BUILD_UNIX_TIME = 1773350293101L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
--- comp/2026/src/main/java/frc/robot/Robot.java
+++ comp/2026/src/main/java/frc/robot/Robot.java
@@ -8,6 +8,7 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.Threads;
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import org.littletonrobotics.junction.LogFileUtil;
@@ -18,14 +19,18 @@ import org.littletonrobotics.junction.wpilog.WPILOGReader;
 import org.littletonrobotics.junction.wpilog.WPILOGWriter;
 
 /**
- * The VM is configured to automatically run this class, and to call the functions corresponding to
- * each mode, as described in the TimedRobot documentation. If you change the name of this class or
- * the package after creating this project, you must also update the build.gradle file in the
+ * The VM is configured to automatically run this class, and to call the
+ * functions corresponding to
+ * each mode, as described in the TimedRobot documentation. If you change the
+ * name of this class or
+ * the package after creating this project, you must also update the
+ * build.gradle file in the
  * project.
  */
 public class Robot extends LoggedRobot {
   private Command autonomousCommand;
   private RobotContainer robotContainer;
+  Timer m_gcTimer = new Timer();
 
   public Robot() {
     // Record metadata
@@ -70,6 +75,8 @@ public class Robot extends LoggedRobot {
     // Instantiate our RobotContainer. This will perform all our button bindings,
     // and put our autonomous chooser on the dashboard.
     robotContainer = new RobotContainer();
+
+    m_gcTimer.start();
   }
 
   /** This function is called periodically during all modes. */
@@ -92,15 +99,23 @@ public class Robot extends LoggedRobot {
 
   /** This function is called once when the robot is disabled. */
   @Override
-  public void disabledInit() {}
+  public void disabledInit() {
+  }
 
   /** This function is called periodically when disabled. */
   @Override
   public void disabledPeriodic() {
     robotContainer.vision.periodic();
+    // run the garbage collector every 5 seconds
+    if (m_gcTimer.advanceIfElapsed(5)) {
+      System.gc();
+    }
   }
 
-  /** This autonomous runs the autonomous command selected by your {@link RobotContainer} class. */
+  /**
+   * This autonomous runs the autonomous command selected by your
+   * {@link RobotContainer} class.
+   */
   @Override
   public void autonomousInit() {
     autonomousCommand = robotContainer.getAutonomousCommand();
@@ -113,7 +128,12 @@ public class Robot extends LoggedRobot {
 
   /** This function is called periodically during autonomous. */
   @Override
-  public void autonomousPeriodic() {}
+  public void autonomousPeriodic() {
+    // run the garbage collector every 5 seconds
+    if (m_gcTimer.advanceIfElapsed(5)) {
+      System.gc();
+    }
+  }
 
   /** This function is called once when teleop is enabled. */
   @Override
@@ -129,7 +149,12 @@ public class Robot extends LoggedRobot {
 
   /** This function is called periodically during operator control. */
   @Override
-  public void teleopPeriodic() {}
+  public void teleopPeriodic() {
+    // run the garbage collector every 5 seconds
+    if (m_gcTimer.advanceIfElapsed(5)) {
+      System.gc();
+    }
+  }
 
   /** This function is called once when test mode is enabled. */
   @Override
@@ -140,13 +165,24 @@ public class Robot extends LoggedRobot {
 
   /** This function is called periodically during test mode. */
   @Override
-  public void testPeriodic() {}
+  public void testPeriodic() {
+    // run the garbage collector every 5 seconds
+    if (m_gcTimer.advanceIfElapsed(5)) {
+      System.gc();
+    }
+  }
 
   /** This function is called once when the robot is first started up. */
   @Override
-  public void simulationInit() {}
+  public void simulationInit() {
+  }
 
   /** This function is called periodically whilst in simulation. */
   @Override
-  public void simulationPeriodic() {}
+  public void simulationPeriodic() {
+    // run the garbage collector every 5 seconds
+    if (m_gcTimer.advanceIfElapsed(5)) {
+      System.gc();
+    }
+  }
 }
