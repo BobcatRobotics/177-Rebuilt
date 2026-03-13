0ab01016f1ac234395630e7fd5c2b63206820cc2
SR34862 <SR34862@apps.southwindsorschools.org>
2026-03-12 21:06:50 -0400

    better working auto (tuning the path)

--- comp/2026/src/main/java/frc/robot/BuildConstants.java
+++ comp/2026/src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 148;
-  public static final String GIT_SHA = "25125a2fbe9ec64e2ce363c63f5c30a2bdfa1b00";
-  public static final String GIT_DATE = "2026-03-12 18:29:52 EDT";
+  public static final int GIT_REVISION = 149;
+  public static final String GIT_SHA = "7716454a06ebc80f70e342257626ccd97ab766d1";
+  public static final String GIT_DATE = "2026-03-12 20:26:44 EDT";
   public static final String GIT_BRANCH = "clean-project";
-  public static final String BUILD_DATE = "2026-03-12 20:23:48 EDT";
-  public static final long BUILD_UNIX_TIME = 1773361428208L;
+  public static final String BUILD_DATE = "2026-03-12 21:00:43 EDT";
+  public static final long BUILD_UNIX_TIME = 1773363643230L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
