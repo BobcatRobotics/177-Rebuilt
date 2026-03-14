4f6b6c080d5e24711ab8b4cd6bc258cc9cc7dda0
SR34862 <SR34862@apps.southwindsorschools.org>
2026-03-14 18:52:33 -0400

    Changes to auto end positions

--- comp/2026/src/main/java/frc/robot/BuildConstants.java
+++ comp/2026/src/main/java/frc/robot/BuildConstants.java
@@ -7,13 +7,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 161;
-  public static final String GIT_SHA = "879b4ebdff6fd54db87e3a54926c5385d729317b";
-  public static final String GIT_DATE = "2026-03-14 16:16:14 EDT";
+  public static final int GIT_REVISION = 162;
+  public static final String GIT_SHA = "ba99f54a5a211e44613e8d2af00e7cd7bf690127";
+  public static final String GIT_DATE = "2026-03-14 17:02:49 EDT";
   public static final String GIT_BRANCH = "clean-project";
-  public static final String BUILD_DATE = "2026-03-14 16:17:01 EDT";
-  public static final long BUILD_UNIX_TIME = 1773519421080L;
-  public static final int DIRTY = 0;
+  public static final String BUILD_DATE = "2026-03-14 18:51:34 EDT";
+  public static final long BUILD_UNIX_TIME = 1773528694684L;
+  public static final int DIRTY = 1;
 
   private BuildConstants(){}
 }
--- comp/2026/src/main/java/frc/robot/subsystems/vision/VisionConstants.java
+++ comp/2026/src/main/java/frc/robot/subsystems/vision/VisionConstants.java
@@ -29,8 +29,8 @@ public class VisionConstants {
       new Transform3d(-0.2, 0.0, 0.2, new Rotation3d(0.0, -0.4, Math.PI));
 
   // Basic filtering thresholds
-  public static double maxAmbiguity = 0.3;
-  public static double maxZError = 0.75;
+  public static double maxAmbiguity = 99.0;
+  public static double maxZError = 1.0;
 
   // Standard deviation baselines, for 1 meter distance and 1 tag
   // (Adjusted automatically based on distance and # of tags)
