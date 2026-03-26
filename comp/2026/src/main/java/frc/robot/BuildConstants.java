aa845b72120ddaf47b7d1f732e182d102e113136
Account <accent-truffle.9w@icloud.com>
2026-03-25 19:38:01 -0500

    Constants & PIDS.

--- comp/2026/src/main/java/frc/robot/BuildConstants.java
+++ comp/2026/src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 194;
-  public static final String GIT_SHA = "e3a44694af16a3c03874eb52d51ba4c3d0e152e0";
-  public static final String GIT_DATE = "2026-03-23 10:17:56 EDT";
-  public static final String GIT_BRANCH = "hashmap";
-  public static final String BUILD_DATE = "2026-03-23 14:53:12 EDT";
-  public static final long BUILD_UNIX_TIME = 1774291992557L;
+  public static final int GIT_REVISION = 195;
+  public static final String GIT_SHA = "39a768e2bf695ad5c7512356bdc035e3fa60d4c2";
+  public static final String GIT_DATE = "2026-03-23 16:10:14 EDT";
+  public static final String GIT_BRANCH = "shooter-retuning";
+  public static final String BUILD_DATE = "2026-03-25 20:35:41 EDT";
+  public static final long BUILD_UNIX_TIME = 1774485341404L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
