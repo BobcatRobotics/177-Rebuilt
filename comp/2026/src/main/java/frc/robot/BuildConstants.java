3ef533b33192cc632928ea7bd5300e9ecc7255e8
Account <accent-truffle.9w@icloud.com>
2026-03-27 07:02:44 -0400

    Corrects BallPath
    Ball path must have correct units.

--- comp/2026/src/main/java/frc/robot/BuildConstants.java
+++ comp/2026/src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 206;
-  public static final String GIT_SHA = "1c0c1ad1011b9fd02b8cfea6427a3618801e58fd";
-  public static final String GIT_DATE = "2026-03-26 19:18:47 EDT";
+  public static final int GIT_REVISION = 209;
+  public static final String GIT_SHA = "54a9438a4b91cfa46b90146500a9d7a12902c96a";
+  public static final String GIT_DATE = "2026-03-27 02:44:03 EDT";
   public static final String GIT_BRANCH = "akash_hartford_autos";
-  public static final String BUILD_DATE = "2026-03-26 22:24:39 EDT";
-  public static final long BUILD_UNIX_TIME = 1774578279672L;
+  public static final String BUILD_DATE = "2026-03-27 06:59:24 EDT";
+  public static final long BUILD_UNIX_TIME = 1774609164031L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
