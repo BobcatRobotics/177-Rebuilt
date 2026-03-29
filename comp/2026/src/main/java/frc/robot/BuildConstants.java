1b251ce7dd75a4d3fe08ade7fd3247f648ef2037
h3L1X <116317505+h3L1X@users.noreply.github.com>
2026-03-29 11:51:15 -0400

    Edited paths and final auto aim changes

--- comp/2026/src/main/java/frc/robot/BuildConstants.java
+++ comp/2026/src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 216;
-  public static final String GIT_SHA = "ac6c2ab579dc2d8c0338094a940bb6a559d2ef41";
-  public static final String GIT_DATE = "2026-03-28 20:02:02 EDT";
+  public static final int GIT_REVISION = 217;
+  public static final String GIT_SHA = "f063185999fdf2e2c0c0e31bb9c6e169d56c077b";
+  public static final String GIT_DATE = "2026-03-29 08:18:54 EDT";
   public static final String GIT_BRANCH = "akash_hartford_autos";
-  public static final String BUILD_DATE = "2026-03-29 08:18:25 EDT";
-  public static final long BUILD_UNIX_TIME = 1774786705871L;
+  public static final String BUILD_DATE = "2026-03-29 11:47:43 EDT";
+  public static final long BUILD_UNIX_TIME = 1774799263459L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
