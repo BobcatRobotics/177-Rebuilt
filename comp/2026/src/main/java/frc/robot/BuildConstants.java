cb5b2845257ece4e350d7d6af2b6bdce42bd491e
h3L1X <116317505+h3L1X@users.noreply.github.com>
2026-04-04 16:22:46 -0400

    changed some paths

--- comp/2026/src/main/java/frc/robot/BuildConstants.java
+++ comp/2026/src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 229;
-  public static final String GIT_SHA = "93614f2e79ba7f412b083a6ad0b0ae225af53850";
-  public static final String GIT_DATE = "2026-04-04 14:33:10 EDT";
-  public static final String GIT_BRANCH = "21-set-and-change-imu-mode-during-enable-disable-methods";
-  public static final String BUILD_DATE = "2026-04-04 15:35:11 EDT";
-  public static final long BUILD_UNIX_TIME = 1775331311292L;
+  public static final int GIT_REVISION = 231;
+  public static final String GIT_SHA = "f8667aed25b18f1a65f12068f4aecaeb10b7fffa";
+  public static final String GIT_DATE = "2026-04-04 15:41:14 EDT";
+  public static final String GIT_BRANCH = "17-autos-vision";
+  public static final String BUILD_DATE = "2026-04-04 16:04:40 EDT";
+  public static final long BUILD_UNIX_TIME = 1775333080732L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
