93614f2e79ba7f412b083a6ad0b0ae225af53850
AashrithG <gummadiaashrith@gmail.com>
2026-04-04 14:33:10 -0400

    intake LL moved

--- comp/2026/src/main/java/frc/robot/subsystems/vision/VisionConstants.java
+++ comp/2026/src/main/java/frc/robot/subsystems/vision/VisionConstants.java
@@ -24,7 +24,8 @@ public class VisionConstants {
   // side is right-positive
   public static limelightConstants[] cameraConstants = {
     new limelightConstants("limelight-shooter", 0.42, 0.0, 0.2, 0.0, 27, 0.0), 
-    new limelightConstants("limelight-intake", 0.08586724, 0.0, 0.53385466, 0.0, 23, 180),
+    //new limelightConstants("limelight-intake", 0.08586724, 0.0, 0.53385466, 0.0, 23, 180),
+    new limelightConstants("limelight-intake", 0.1838452, 0.2422652, 0.492506, 0.0, 26, 180),
     new limelightConstants("limelight-fleft", 0.381, -0.2477516, 0.1929384, 0.0, 27, 90),
     new limelightConstants("limelight-fright", 0.381, 0.2477516, 0.1929384, 0.0, 27, -90)
 };
