1cfa346047b4197156a90aa576aa3fc58ec9a547
GIRPRA <girishprasad102@gmail.com>
2026-04-06 22:47:37 -0400

    Fixed incorrect NamedCommands

--- comp/2026/src/main/java/frc/robot/BuildConstants.java
+++ comp/2026/src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 253;
-  public static final String GIT_SHA = "75af15a3988169f5d91635dc0961821ae8ffa897";
-  public static final String GIT_DATE = "2026-04-05 15:07:34 EDT";
+  public static final int GIT_REVISION = 254;
+  public static final String GIT_SHA = "bb29731becc285155f129bfbcd58800310dccf60";
+  public static final String GIT_DATE = "2026-04-06 20:44:34 EDT";
   public static final String GIT_BRANCH = "ApplesauceAutos";
-  public static final String BUILD_DATE = "2026-04-06 20:07:30 EDT";
-  public static final long BUILD_UNIX_TIME = 1775520450792L;
+  public static final String BUILD_DATE = "2026-04-06 22:43:20 EDT";
+  public static final long BUILD_UNIX_TIME = 1775529800335L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
