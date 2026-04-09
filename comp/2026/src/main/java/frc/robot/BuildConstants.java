65dfc53db38368542c7f249ef1584c878c5c0459
GIRPRA <girishprasad102@gmail.com>
2026-04-09 18:29:57 -0400

    Added sequential group to end of applesauce autos

--- comp/2026/src/main/java/frc/robot/BuildConstants.java
+++ comp/2026/src/main/java/frc/robot/BuildConstants.java
@@ -7,13 +7,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 257;
-  public static final String GIT_SHA = "4d9b4fbb5bb45d4f0c88a7d8f38ee8beab2e0a9f";
-  public static final String GIT_DATE = "2026-04-07 12:11:51 EDT";
+  public static final int GIT_REVISION = 260;
+  public static final String GIT_SHA = "5f736088c0c949e4da7bfe028837d3bb1eb0c229";
+  public static final String GIT_DATE = "2026-04-07 20:28:24 EDT";
   public static final String GIT_BRANCH = "ApplesauceAutos";
-  public static final String BUILD_DATE = "2026-04-07 18:16:06 EDT";
-  public static final long BUILD_UNIX_TIME = 1775600166331L;
-  public static final int DIRTY = 1;
+  public static final String BUILD_DATE = "2026-04-09 18:20:26 EDT";
+  public static final long BUILD_UNIX_TIME = 1775773226231L;
+  public static final int DIRTY = 0;
 
   private BuildConstants(){}
 }
