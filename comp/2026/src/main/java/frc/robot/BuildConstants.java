9b8e107f669f2e288eec37f862044fc7ab7a3293
h3L1X <116317505+h3L1X@users.noreply.github.com>
2026-04-11 10:51:26 -0400

    apple sauce fixes

--- comp/2026/src/main/java/frc/robot/BuildConstants.java
+++ comp/2026/src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 279;
-  public static final String GIT_SHA = "4bea12dffadb1840c16330066a5451c002b076d2";
-  public static final String GIT_DATE = "2026-04-10 23:57:47 EDT";
-  public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2026-04-11 10:12:42 EDT";
-  public static final long BUILD_UNIX_TIME = 1775916762062L;
+  public static final int GIT_REVISION = 280;
+  public static final String GIT_SHA = "b2b098e9083426cdbfbb1899a2972b6104565a44";
+  public static final String GIT_DATE = "2026-04-11 10:24:24 EDT";
+  public static final String GIT_BRANCH = "Pre-WPI-Practice";
+  public static final String BUILD_DATE = "2026-04-11 10:43:17 EDT";
+  public static final long BUILD_UNIX_TIME = 1775918597631L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
