cf7d5c83b523ac68655619404ebaf61080dd3d5c
h3L1X <116317505+h3L1X@users.noreply.github.com>
2026-04-12 15:35:11 -0400

    Outta the way on the outpost side works, will mirror

--- comp/2026/src/main/java/frc/robot/BuildConstants.java
+++ comp/2026/src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 280;
-  public static final String GIT_SHA = "b2b098e9083426cdbfbb1899a2972b6104565a44";
-  public static final String GIT_DATE = "2026-04-11 10:24:24 EDT";
-  public static final String GIT_BRANCH = "Pre-WPI-Practice";
-  public static final String BUILD_DATE = "2026-04-11 10:43:17 EDT";
-  public static final long BUILD_UNIX_TIME = 1775918597631L;
+  public static final int GIT_REVISION = 283;
+  public static final String GIT_SHA = "df224d3216d462438dc92301a9ac7743df1a0d12";
+  public static final String GIT_DATE = "2026-04-12 12:58:08 EDT";
+  public static final String GIT_BRANCH = "wpi-field";
+  public static final String BUILD_DATE = "2026-04-12 15:31:42 EDT";
+  public static final long BUILD_UNIX_TIME = 1776022302605L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
