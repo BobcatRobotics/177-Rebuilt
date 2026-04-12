df3aa9a7a8a37b69bfc14e20d0e02525b936cb70
Account <accent-truffle.9w@icloud.com>
2026-04-12 15:50:39 -0400

    Mirrored autos for out of the way.

--- comp/2026/src/main/java/frc/robot/BuildConstants.java
+++ comp/2026/src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 283;
-  public static final String GIT_SHA = "df224d3216d462438dc92301a9ac7743df1a0d12";
-  public static final String GIT_DATE = "2026-04-12 12:58:08 EDT";
+  public static final int GIT_REVISION = 284;
+  public static final String GIT_SHA = "cf7d5c83b523ac68655619404ebaf61080dd3d5c";
+  public static final String GIT_DATE = "2026-04-12 15:35:11 EDT";
   public static final String GIT_BRANCH = "wpi-field";
-  public static final String BUILD_DATE = "2026-04-12 15:31:42 EDT";
-  public static final long BUILD_UNIX_TIME = 1776022302605L;
+  public static final String BUILD_DATE = "2026-04-12 15:44:56 EDT";
+  public static final long BUILD_UNIX_TIME = 1776023096735L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
